dbdaf81faf987c2d0681788ea6a322bf2db0aa05
andreaconv <andrea2convertini@gmail.com>
2023-09-13 17:44:31 +0200

    cibi preferiti

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -1,8 +1,16 @@
 package org.lessons.java.array;
 
+import java.util.Arrays;
+
 public class CibiPreferiti {
 	public static void main(String[] args) {
-		System.out.println("prova");
+		
+		String[] cibiPreferiti = {"pasta", "carne", "torta di mele", "pizza", "pesce", "verdure grigliate", "fagioli", "cavolfiore"};
+		
+		System.out.println("lunghezza array: " + cibiPreferiti.length);
+		System.out.println("cibo preferito: " + cibiPreferiti[0]);
+		System.out.println("cibo sconsigliato: " + cibiPreferiti[cibiPreferiti.length - 1]);
+		System.out.println("cibo preferito: " + cibiPreferiti[cibiPreferiti.length / 2]);
 	}
 
 }
